04741ae888d72592c54f63ea6717bbe5d0492829
Caleb Phillips <calebrphillips@gmail.com>
2017-01-28 15:15:25 -0500

    Simplify callback args

--- scoringFunctions.js
+++ scoringFunctions.js
@@ -3,13 +3,13 @@
 function getRedditScore(submission_id, callback) {
     var serverUrl = 'https://massive-waffle.herokuapp.com/reddit/'
     $.getJSON(server_url + submission_id, function( data ) {
-        callback(submission_id, data)
+        callback(data)
     }); 
 }
 
 function getHNScore(submission_id, callback) {
     var serverUrl = 'https://massive-waffle.herokuapp.com/hn/'
     $.getJSON(server_url + submission_id, function( data ) {
-        callback(submission_id, data)
+        callback(data)
     }); 
 }
