ceaa13c7cb72d4a90fb3dd3eab7699a78f217ae5
eamal27 <elias.amal27@gmail.com>
2017-01-28 18:13:09 -0500

    reddit highlighting works

--- reddit-highlight.js
+++ reddit-highlight.js
@@ -1,55 +1,68 @@
 $( document  ).ready(function() {
 
-	init();
+		init();
 
 });
 
 function init() {
-	console.log("highlight begin");
-	addSentimentBars();
+		console.log("highlight begin");
+		addSentimentBars();
 
 }
 
 function addSentimentBars() {
-	var reddit_posts = $(".thing");
+var reddit_posts = $(".thing");
 
-	reddit_posts.each(function() {
-		$(this).css("position","relative");
-		$(this).append("<div class='sbar' id='"+getPostId($(this))+"'></div");
-	});
+		reddit_posts.each(function() {
+				var rp = $(this);
+				var rp_id = getPostId(rp);
+				var rp_color = getPostColor(rp_id);
+				rp
+						.css("position","relative")
+						.append("<div class='sbar' id='"+rp_id+"'></div");
 
-	var sentiment_meters = $(".sbar");
-	sentiment_meters.each(function() {
-		$(this).css({
-			"background": "chartreuse",
-			"width": "8px",
-			"height": "100%",
-			"position": "absolute",
-			"top": "0",
-			"left": "-4px"
+				var rp_meter = rp.find(".sbar");
+				rp_meter.css({
+				//						"background": rp_color,
+						"width": "7px",
+						"height": "100%",
+						"position": "absolute",
+						"top": "0",
+						"left": "-4px"
+				});
+
+				if(rp_meter.parent().hasClass("reddit-link")) {
+						rp_meter.css({
+								"width": "4px",
+								"left": "-5px"
+						});
+				}
+
+				getRedditScore(rp_id, function(result) {
+						result = result || {};
+						console.log(rp_id + ":" + (result.score || "noresult"));
+						rp_meter.css("background",getPostColor(result.score));
+				});
+
 		});
-
-		if($(this).parent().hasClass("reddit-link")) {
-			$(this).css({
-				"width": "4px",
-				"left": "-5px"
-			});
-		}
-	});
 }
 
 function getPostId(e) {
-	var id = e.attr('class');
-	id_ = id.split("id-t3_");
-	id__ = id_[1].split(" ");
-	
-	return id__[0]
+		var id = e.attr('class');
+		id_ = id.split("id-t3_");
+		id__ = id_[1].split(" ");
+
+		return id__[0]
 }
 
-function getRedditScore(submission_id, callback) {
-	var serverUrl = 'https://massive-waffle.herokuapp.com/reddit/'
-	$.getJSON(server_url + submission_id, function( data  ) {
-		callback(data)
-	}); 
+function getPostColor(rp_score) {
+		var score = (rp_score + 1) /2;
+		return d3.interpolateRdBu(score)
 }
 
+function getRedditScore(submissionId, callback) {
+		var serverUrl = 'https://massive-waffle.herokuapp.com/reddit/'
+		$.getJSON(serverUrl + submissionId, function( data  ) {
+				callback(data)
+		}); 
+}
