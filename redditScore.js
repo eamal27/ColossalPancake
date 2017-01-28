dcf9ce8c8713528eab0383ff3727f64d8502090a
Caleb Phillips <calebrphillips@gmail.com>
2017-01-28 14:48:44 -0500

    Name the funcing fucktion

--- redditScore.js
+++ redditScore.js
@@ -1,7 +1,7 @@
 
 var serverUrl = 'localhost:5000/reddit/'
 
-function(submission_id, callback) {
+function getRedditScore(submission_id, callback) {
     $.get(server_url + submission_id, function( score ) {
         callback(submission_id, score)
     }); 
